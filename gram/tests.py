5667246ceb009bee7339ecbfb2e9550c0f77d6d7
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-11-24 18:27:49 +0300

    Create Follow test case Create test case for instance of the Follow class Create test case for the get following class method for the Follow class

--- gram/tests.py
+++ gram/tests.py
@@ -101,8 +101,7 @@ class PostTestClass(TestCase):
         Method that creates an instance of Profile class
         '''
 
-        # Create and save
-
+        # Create a Post instance
         self.new_post = Post(caption ='Python James is Muriuki who wrote Python content for Moringa School')
 
     def test_instance(self):
@@ -131,7 +130,7 @@ class PostTestClass(TestCase):
 
         self.test_profile = Profile(user=self.jane,bio="Another Profile")
 
-        self.test_profile = Profile(user=self.jane,bio="Another Profile")
+        self.test_post = Post(user=self.jane,caption="Another Profile")
 
         gotten_profile = Profile.get_single_profile(self.jane.id)
 
@@ -139,24 +138,47 @@ class PostTestClass(TestCase):
 
         self.assertTrue( len(gotten_profile) != len(profiles))
 
-# class FollowTestClass(TestCase):
-#     '''
-#     Test case for the Profile class
-#     '''
-#     def setUp(self):
-#         '''
-#         Method that creates an instance of Profile class
-#         '''
+class FollowTestClass(TestCase):
+    '''
+    Test case for the Profile class
+    '''
+    def test_instance(self):
+        '''
+        Test case to check if self.new_post in an instance of Post class
+        '''
+        self.james = User(username="kiki")
+        self.james.save()
 
-#         # Create and save
+        self.jane = User(username="ja-ne")
+        self.jane.save()
 
-#         self.new_post = Post(caption ='Python James is Muriuki who wrote Python content for Moringa School')
+        self.test_profile = Profile(user=self.jane,bio="Another Profile")
 
-#     def test_instance(self):
-#         '''
-#         Test case to check if self.new_post in an instance of Post class
-#         '''
-#         self.assertTrue( isinstance(self.new_post, Post) )
+        self.new_follow = Follow(user=self.jane, profile=self.test_profile)
+
+        self.assertTrue( isinstance(self.new_follow, Follow) )
+
+    def test_get_following(self):
+        '''
+        Test case to test get following is getting profiles a specific user is following
+        '''
+        self.james = User(username="kiki")
+        self.james.save()
+
+        self.jane = User(username="ja-ne")
+        self.jane.save()
+
+        self.test_profile = Profile(user=self.jane,bio="Another Profile")
+
+        self.test_post = Post(user=self.jane,caption="Another Profile")
+
+        self.new_follow = Follow(user=self.jane, profile=self.test_profile)
+
+        gotten_following = Follow.get_following(self.jane.id)
+
+        followings = Follow.objects.all()
+
+        self.assertTrue( len(gotten_following) == len(followings))
 
 
 
