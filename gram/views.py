f031b8323c4fce9207edc66563a8533a10c19b12
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-11-24 14:40:48 +0300

    Update timeline view function Update timeline view function to only de accessible to registered users Create navbar with site icon, site name, user icaon and logout link Update timeline page to display logged in user's username Create style sheet for the navbar.html Change background color and border color of the navbar Change margin of the navbar with the top of the page Style links to remain black on hover

--- gram/views.py
+++ gram/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-# @login_required(login_url='/accounts/login')
+@login_required(login_url='/accounts/register')
 def timeline(request):
     '''
     View function to display the timeline page for an authenticated logged in user
     '''
+    current_user = request.user
     title = 'Home'
     message = 'Timeline Page'
-    return render(request, 'all-posts/timeline.html', {"title": title, "message": message})
+    return render(request, 'all-posts/timeline.html', {"title": title, "message": message, "user":current_user})
 
 
