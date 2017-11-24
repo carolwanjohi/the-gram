5b3d6febb117f6bff27222d2b3dff73bf6fd35f6
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-11-24 16:42:53 +0300

    Create Profile model Create Profile model to create a users profile Create test case for the instance for the Profile class Create test case for the get profiles class method for the Profile class Create test case for the get single profile class method for the Profile class Add images to be set as default profile image

--- gram/models.py
+++ gram/models.py
@@ -1,3 +1,76 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime as dt
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+# Default image for a new profile
+DEFAULT = 'static/images/kakashi.jpg'
 
 # Create your models here.
+class Profile(models.Model):
+    '''
+    Class to define a user's profile
+    '''
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    profile_pic = models.ImageField(upload_to="profile-pic/", blank=True, default=DEFAULT)
+
+    def __str__(self):
+        '''
+        Display for profiles in profile table
+        '''
+        return self.user.username
+
+    @classmethod
+    def get_profiles(cls):
+        '''
+        Fucntion that gets all the profiles in the app
+
+        Return
+            profiles : list of all Profile obejcts in the database
+        '''
+        profiles = Profile.objects.all()
+        return profiles
+
+    @classmethod
+    def get_single_profile(cls,user_id):
+        '''
+        Function that get's the profile of the specified user
+
+        Args:
+            user_id : id of specific user
+
+        Return:
+            single_profile : single Profile object belonging to user with specified id
+        '''
+        single_profile = Profile.objects.filter(user=user_id).all()
+        return single_profile
+
+    # @classmethod
+    # def set_profile_pic_to_default(self):
+    #     '''
+    #     Function that set's a profile pictue back to the default one
+    #     '''
+    #     self.profile_pic.delete(save=False)  
+    #     self.profile_pic = DEFAULT
+    #     self.save()
+
+# Create Profile when creating a User
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+# Save Profile when saving a User
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+
+
+
+
+
+
+
+
--- gram/tests.py
+++ gram/tests.py
@@ -1,3 +1,58 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Profile
 
 # Create your tests here.
+class ProfileTestClass(TestCase):
+    '''
+    Test case for the Profile class
+    '''
+    def setUp(self):
+        '''
+        Method that creates an instance of Profile class
+        '''
+        self.new_profile = Profile(bio="I am Groot")
+
+    # def tearDown(self):
+    #     '''
+    #     Method to delete instances of models after each test
+    #     '''
+
+    def test_instance(self):
+        '''
+        Test case to check if self.new_profile in an instance of Profile class
+        '''
+        self.assertTrue( isinstance(self.new_profile, Profile) )
+        User.objects.all().delete()
+
+    def test_get_profiles(self):
+        '''
+        Test case to check if all profiles are gotten from the database
+        '''
+        gotten_profiles = Profile.get_profiles()
+        profiles = Profile.objects.all()
+        self.assertTrue( len(gotten_profiles) == len(profiles))
+
+    def test_get_single_profile(self):
+        '''
+        Test case to check if all profiles are gotten from the database
+        '''
+        self.james = User(username="kiki")
+        self.james.save()
+
+        self.jane = User(username="ja-ne")
+        self.jane.save()
+
+        self.test_profile = Profile(user=self.jane,bio="Another Profile")
+
+        gotten_profile = Profile.get_single_profile(self.jane.id)
+
+        profiles = Profile.objects.all()
+
+        self.assertTrue( len(gotten_profile) != len(profiles))
+
+    
+
+
+
+
