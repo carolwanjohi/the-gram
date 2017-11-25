da81d8fe9dbf69334b82609174d8553816e5de7d
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-11-25 10:02:41 +0300

    Update profile view function Update profile view function to lead a logged in user to their profile Update my-profile.html to have a div where posts will be displayed Update css file for my-profile.html to style the row displaying a users profile picture and their name

--- gram/views.py
+++ gram/views.py
@@ -17,28 +17,34 @@ def timeline(request):
 @login_required(login_url='/accounts/register')
 def profile(request,id):
     '''
-    View function to display the profile of either the logged in user or logged in user viewing another person's profile
+    View function to display the profile of the logged in user when they click on the user icon
     '''
     current_user = request.user
-    single_profile = Profile.objects.get(user=current_user.id)
+    try:
+        single_profile = Profile.objects.get(user=current_user.id)
+        title = f'{current_user.username}\'s'
+        message = f'{current_user.username}\'s profile'
+        return render(request, 'all-posts/my-profile.html', {"title":title,"current_user":current_user,"message":message})
+    except DoesNotExists:
+        raise Http404()
 
-    print(single_profile)
-    print(current_user)
-    title = f'{single_profile.user.username}'
+    # print(single_profile)
+    # print(current_user)
+    # title = f'{single_profile.user.username}'
 
-    if single_profile.user.id != current_user.id :
+    # if single_profile.user.id != current_user.id :
             
-        '''
-        If the logged in user is not the owner of the profile display the following
-        '''
-        message = f'{current_user.username} is visiting {single_profile.user.username}\'s profile'
-        return render(request, 'all-posts/following-profile.html', {"title":title,"current_user":current_user,"message":message})
+    #     '''
+    #     If the logged in user is not the owner of the profile display the following
+    #     '''
+    #     message = f'{current_user.username} is visiting {single_profile.user.username}\'s profile'
+    #     return render(request, 'all-posts/following-profile.html', {"title":title,"current_user":current_user,"message":message})
 
-    '''
-    If the logged in user is the owner of the profile display the following
-    '''
-    message = f'{current_user.username}\'s profile'
-    return render(request, 'all-posts/my-profile.html', {"title":title,"current_user":current_user,"message":message})
+    # '''
+    # If the logged in user is the owner of the profile display the following
+    # '''
+    # message = f'{current_user.username}\'s profile'
+    # return render(request, 'all-posts/my-profile.html', {"title":title,"current_user":current_user,"message":message})
 
 
 
